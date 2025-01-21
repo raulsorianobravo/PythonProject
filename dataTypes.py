e0fbf3fd5b0f4bd45562dd1928b25f52b6cc0f50
raulsb <rsorianobravo@gmail.com>
2025-01-21 12:17:57 +0100

    DataTypes Basic

---
+++ dataTypes.py
@@ -0,0 +1,28 @@
+#Variables
+
+value1 = 33
+value2 = 2
+total = value1 + value2
+print(total)
+
+#------------------------
+items = 2
+value = 56
+price_end = items * value
+print(price_end)
+
+#Multiple vars
+print(total, price_end)
+
+#Assing var
+x = 67
+y = 6
+z = 66
+print(x,y,z)
+
+#Add and concatenate and types
+x = 10
+y = "10"
+print(x+x,y+y)
+print(type(x),type(y))
+z= 6.4
