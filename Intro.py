3ad2e9248417dde1e7f569683054acf338ebac1b
raulsb <rsorianobravo@gmail.com>
2025-01-21 11:52:22 +0100

    Operators

--- Intro.py
+++ Intro.py
@@ -10,3 +10,21 @@
 #5. git remote add origin <URL>
 #6. git branch -M main --> to match with GitHub structure
 #7. git push origin -f -u origin main
+
+#Python terminal 
+#py -3 on windows
+#python3
+
+#py -3 .\file.py
+
+
+print(56 + 88)
+print(33 - 44)
+print(3 * 55)
+print(33 / 11)
+#Floor Division
+print(22 // 3)
+#Modulus
+print(5 % 2)
+#Exponent
+print(2 ** 3)
