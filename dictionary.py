ef146f1ab9485c55fea9431c1870e4c47d69d79d
raulsb <rsorianobravo@gmail.com>
2025-01-21 14:32:30 +0100

    Dictionaries

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+#Dictionaries
+DictExample = {"A":55,"B":88,"C":9}
+
+_sum = sum(DictExample.values())
+
+print(_sum)
+
+print(DictExample.values())
+print(DictExample.keys())
+print(DictExample)
+
+#Exercises
+day_temp = {"morning":9.0,"noon":14.4,"evening":16.0}
