5658f08b3c4a1fa79caac264f378cc554638c05c
raulsb <rsorianobravo@gmail.com>
2025-01-21 11:42:31 +0100

    Instruction to git remote

--- Intro.py
+++ Intro.py
@@ -1,1 +1,12 @@
 #Introduction
+#Create an empty folder
+#Create a file .py
+
+#Git Bash
+#1. Go to the folder
+#2. git init
+#3. git add * or git add .
+#4. git commit -m "Initial Commit"
+#5. git remote add origin <URL>
+#6. git branch -M main --> to match with GitHub structure
+#7. git push origin -f -u origin main
