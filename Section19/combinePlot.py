ee58c88e7095bd379514d4cb5f73bee7f416d551
raulsb <rsorianobravo@gmail.com>
2025-02-18 13:23:06 +0100

    combien plots

--- Section19/combinePlot.py
+++ Section19/combinePlot.py
@@ -7,7 +7,10 @@ p.title.text_font_style = "italic"
 p.yaxis.minor_tick_line_color = "Yellow"
 p.xaxis.axis_label = "Times"
 p.yaxis.axis_label = "Value"
-p.circle([1,2,3,4,5], [5,6,5,5,3], size = [i*2 for i in [8,12,14,15,20]], color="red", alpha=0.5)
-output_file("Scatter_plotting.html")
-show(p)
 
+p.line([1,2,3,4,5], [5,6,5,5,3], line_width=2, color="red", alpha=0.5)
+p.circle([1,2,3,4,5], [5,6,5,5,3], size = [i*2 for i in [8,12,14,15,20]], color="red", alpha=0.5)
+
+output_file("Scatter_plotting.html")
+
+show(p)
