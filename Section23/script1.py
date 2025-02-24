6c2b237015ffcc811f80a69f4fd0449923ab61b0
raulsb <rsorianobravo@gmail.com>
2025-02-24 10:10:07 +0100

    inputs and functions tkinter

--- Section23/script1.py
+++ Section23/script1.py
@@ -2,12 +2,22 @@ from tkinter import *
 
 window = Tk()
 
-b1 = Button(window, text="Run")
+def kmToMiles():
+    print(e1_value.get())
+    miles = float(e1_value.get())*1.6
+    t1.insert(END, miles)
+
+b1 = Button(window, text="Run", command=kmToMiles)
 b1.grid(row=0, column=0)
 
-e1 = Entry(window)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
+
 
 t1 = Text(window, height=1, width=50)
 t1.grid(row=0, column=2)
+
+
 window.mainloop()
