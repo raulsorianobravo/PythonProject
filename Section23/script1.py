c26543f8f1b14a68eede9994c80ccfc802a43588
raulsb <rsorianobravo@gmail.com>
2025-02-24 09:58:39 +0100

    GUI app

---
+++ Section23/script1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+window = Tk()
+
+b1 = Button(window, text="Run")
+b1.grid(row=0, column=0)
+
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+t1 = Text(window, height=1, width=50)
+t1.grid(row=0, column=2)
+window.mainloop()
