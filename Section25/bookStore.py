617ffb5d0efc17a97d238bedd86bc3376df8f259
raulsb <rsorianobravo@gmail.com>
2025-02-24 19:37:46 +0100

    BookStore Frontend

---
+++ Section25/bookStore.py
@@ -0,0 +1,92 @@
+"""
+Book Program of book information
+
+Info
+
+Functions
+
+"""
+
+from tkinter import *
+import backBookStore
+ 
+# Create an empty Tkinter window
+window=Tk()
+
+# Create a Label widget with "Kg" as label
+l1=Label(window,text="Title")
+l1.grid(row=0,column=0) # The Label is placed in position 0, 0 in the window
+# Create a Label widget with "Kg" as label
+l2=Label(window,text="Author")
+l2.grid(row=0,column=2) # The Label is placed in position 0, 0 in the window
+# Create a Label widget with "Kg" as label
+l3=Label(window,text="Year")
+l3.grid(row=1,column=0) # The Label is placed in position 0, 0 in the window
+# Create a Label widget with "Kg" as label
+l4=Label(window,text="ISBN")
+l4.grid(row=1,column=2) # The Label is placed in position 0, 0 in the window
+
+
+title_text=StringVar()  # Create a special StringVar object
+e1=Entry(window,textvariable=title_text)  # Create an Entry box for users to enter the value
+e1.grid(row=0,column=1)
+
+author_text=StringVar()  # Create a special StringVar object
+e2=Entry(window,textvariable=author_text)  # Create an Entry box for users to enter the value
+e2.grid(row=0,column=3)
+
+year_text=StringVar()  # Create a special StringVar object
+e3=Entry(window,textvariable=year_text)  # Create an Entry box for users to enter the value
+e3.grid(row=1,column=1)
+
+isbn_text=StringVar()  # Create a special StringVar object
+e4=Entry(window,textvariable=isbn_text)  # Create an Entry box for users to enter the value
+e4.grid(row=1,column=3)
+
+list1 = Listbox(window, height=6, width=35)
+list1.grid(row= 2, rowspan = 6, column=0, columnspan=2)
+
+sb1 = Scrollbar(window)
+sb1.grid(row=2, column=2)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+b1=Button(window, text="View all", width=12)
+b2=Button(window, text="Search", width=12)
+b3=Button(window, text="Add", width=12)
+b4=Button(window, text="Update", width=12)
+b5=Button(window, text="Delete", width=12)
+b6=Button(window, text="Close", width=12)
+
+b1.grid(row=2,column=3)
+b2.grid(row=3,column=3)
+b3.grid(row=4,column=3)
+b4.grid(row=5,column=3)
+b5.grid(row=6,column=3)
+b6.grid(row=7,column=3)
+
+
+
+# def from_kg():
+#     # Get user value from input box and multiply by 1000 to get kilograms
+#     gram=float(e2_value.get())*1000
+ 
+#     # Get user value from input box and multiply by 2.20462 to get pounds
+#     pound=float(e2_value.get())*2.20462
+ 
+#     # Get user value from input box and multiply by 35.274 to get ounces
+#     ounce=float(e2_value.get())*35.274
+ 
+#     # Empty the Text boxes if they had text from the previous use and fill them again
+#     t1.delete("1.0", END)  # Deletes the content of the Text box from start to END
+#     t1.insert(END,gram)  # Fill in the text box with the value of gram variable
+#     t2.delete("1.0", END)
+#     t2.insert(END,pound)
+#     t3.delete("1.0", END)
+#     t3.insert(END,ounce)
+ 
+
+ 
+# # This makes sure to keep the main window open
+window.mainloop()
