87b0c15af49fd2a690918ff587d3285b37ae1872
raulsb <rsorianobravo@gmail.com>
2025-02-28 09:13:16 +0100

    Change screens

--- Section27/main.py
+++ Section27/main.py
@@ -5,9 +5,15 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file("./Section27/design.kv")
 
 class LoginScreen(Screen):
+
+    def sign_up(self):
+        print("sign up")
+        self.manager.current = "sign_up_screen"
+
+class SignUpScreen(Screen):
     pass
 
-class RootWidget(Screen):
+class RootWidget(ScreenManager):
     pass
 
 class MainApp(App):
