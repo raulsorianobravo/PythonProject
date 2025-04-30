324e95038d6546d1e5acfd66dd048003643c8d83
raulsb <rsorianobravo@gmail.com>
2025-04-30 10:36:12 +0200

    Add user json

--- Section27/main.py
+++ Section27/main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file("./Section27/design.kv")
 
@@ -14,8 +16,15 @@ class SignUpScreen(Screen):
     
     def add_user(self, uname, pword):
         print(uname, pword)
+        with open("./Section27/user.json") as file:
+            users = json.load(file)
+         
+        users[uname] = {"username":uname, "password":pword, "created": datetime.now().strftime("%Y-%m-%d %H:%M:%S") }
 
+        with open("./Section27/user.json", "w") as file:
+            json.dump(users, file)
 
+        
 class RootWidget(ScreenManager):
     pass
 
