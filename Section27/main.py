dcea74caeac49c6d950c4e1c708ac434991b564d
raulsb <rsorianobravo@gmail.com>
2025-05-05 12:06:18 +0200

    Add quote selection

--- Section27/main.py
+++ Section27/main.py
@@ -1,5 +1,6 @@
 import glob
 from pathlib import Path
+import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -54,9 +55,12 @@ class LoginScreenSuccess(Screen):
 
         available_feelings = [Path(filename).stem for filename in available_feelings]
         if feel in available_feelings:
-            with open(f"/Section27/quotes/{feel}.txt") as file:
+            with open(f"./Section27/quotes/{feel}.txt", encoding="utf8") as file:
                 quotes = file.readlines()
-            print(quotes)    
+            print(quotes)
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling"
 
 class RootWidget(ScreenManager):
     pass
