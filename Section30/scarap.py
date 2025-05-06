61fa0b42af121745e1e4f436f751ec74e4c6dc1d
raulsb <rsorianobravo@gmail.com>
2025-05-06 11:52:02 +0200

    Get other features

--- Section30/scarap.py
+++ Section30/scarap.py
@@ -33,20 +33,24 @@ for item in all:
     try:
         print(item.find("span",{"class":"infoBed"}).find("b").text)    
     except:
-        pass
+        print(None)
     try:
         print(item.find("span",{"class":"infoSqFt"}).find("b").text)    
     except:
-        pass
+        print(None)
     try:
         print(item.find("span",{"class":"infoValueFullBath"}).find("b").text)    
     except:
-        pass
+        print(None)
     try:
         print(item.find("span",{"class":"infoValueHalfBath"}).find("b").text)    
     except:
-        pass
-    print("")
+        print(None)
 
+    for column_group in item.find_all("div",{"class":"columnGroup"}):
+        #print(column_group)
+        for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}), column_group.find_all("span",{"class":"featureName"})):
+            print(feature_group.text, feature_name.text)
+    print("---------------------------------")
 
 
